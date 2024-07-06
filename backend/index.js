1ccf3e8dffe9d427f5e689b06bce116a6793ab93
mehdikert <kertous2002m@outlook.com>
2024-07-06 16:45:57 +0200

    endpoint file updated (index.js) and db config added

---
+++ backend/index.js
@@ -0,0 +1,61 @@
+const express = require('express')
+const app = express()
+const dotenv = require('dotenv')
+dotenv.config()
+const cors = require('cors')
+app.use(cors())
+
+const bodyParser = require('body-parser')
+//
+app.use(bodyParser.json())
+//
+app.use(bodyParser.urlencoded({ extended: true }))
+
+
+
+// CORS configuration (ajout des liens ayant les privilèges d'utiliser l'api)
+const allowedOrigins = [
+    'http://localhost:5173',
+    process.env.BASE_URL
+];
+
+
+app.use(cors({
+    origin: function (origin, callback) {
+        if (allowedOrigins.indexOf(origin) !== -1 || !origin) {
+            callback(null, true);
+        } else {
+            callback(new Error('Not allowed by CORS'));
+        }
+    },
+    methods: ["POST", "GET", "DELETE", "PUT"],
+    credentials: true
+}));
+
+
+app.use(express.json())
+
+
+
+
+// Middleware 404 -- si la route existe pas ça log cette erreur 
+app.use((req, res, next) => {
+    res.status(404).json({ message: 'Route not found' });
+});
+
+
+
+// Connexion a la base de donnée
+
+
+
+// Server mis à l'écoute
+
+app.listen(process.env.PORT, (error) => {
+    if (error) {
+        return console.log('Erreur lors du demarrage du server');
+    }
+    console.log('Demarrage du serveur sur le port : ' + process.env.PORT || 8080);
+})
+
+
