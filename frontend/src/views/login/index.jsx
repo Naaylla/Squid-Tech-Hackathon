7a3819d102a4880bb0b0ed999a4a32aa311a344d
Naaylla <lulita181910@hotmail.fr>
2024-07-07 20:11:13 +0100

    added on-progress login

---
+++ frontend/src/views/login/index.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+export default function Login() {
+    return (
+        <div className="flex w-full h-screen items-center justify-center">
+            <div>
+                <h1>Bienvenue !</h1>
+                <p>Vous n'avez pas de compte ?</p>
+            </div>
+        </div>
+    )
+}
