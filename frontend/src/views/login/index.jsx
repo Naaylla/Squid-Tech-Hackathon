877d953b8311dcb939e39d30a4abf05023e73b20
Naaylla <lulita181910@hotmail.fr>
2024-07-10 00:49:25 +0100

    added login, register, fixed responsive

--- frontend/src/views/login/index.jsx
+++ frontend/src/views/login/index.jsx
@@ -1,16 +1,16 @@
 import React from "react";
 import { Link } from 'react-router-dom';
-import Form from "../../components/Form";
+import Form from "../../components/FormLogin";
 
 export default function Login() {
     return (
         <div className="flex w-full h-screen items-center justify-center">
-            <div className="lg:w-2/4 md:w-6/12 rounded-lg shadow-md bg-white overflow-hidden p-8 flex flex-col items-center justify-center space-y-8 border-2 border-sky-500">
-                <h1 className="text-center font-bold text-4xl">Bienvenue !</h1>
-                <p className="font-light text-lg text-center">
-                    <span className="opacity-60">Vous n'avez pas de compte ? </span>
-                    <Link to="/signup" className="text-blue-500 hover:underline">
-                        S'inscrire
+            <div className="lg:w-2/5 md:w-96 w-96 mt-12 rounded-3xl shadow-md bg-white overflow-hidden p-8 flex flex-col items-center justify-center space-y-8 border-2 border-neutral-400 border-opacity-50">
+                <h1 className="text-center font-bold text-black text-3xl">Bienvenue !</h1>
+                <p className="font-normal text-lg text-center">
+                    <span className="opacity-60 text-black text-base">Vous n’avez pas de compte ? </span>
+                    <Link to="/signup" className="text-black font-semibold text-base hover:underline">
+                        S’inscrire
                     </Link>
                 </p>
                 <Form />
