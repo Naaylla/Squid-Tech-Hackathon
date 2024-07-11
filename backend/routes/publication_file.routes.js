5f1588d2540af5f818182979aeffdf2b861d2b12
mehdikert <kertous2002m@outlook.com>
2024-07-12 01:06:01 +0200

    dernier commit

--- backend/routes/publication_file.routes.js
+++ backend/routes/publication_file.routes.js
@@ -1,12 +1,13 @@
 const { get_all_publication_file, get_publication_file, add_publication_file, delete_publication_file, update_publication_file } = require('../controllers/publication_file.controller')
+const { verify_Token } = require('../middlewares/auth.middleware')
 
 const router_publication_file = require('express').Router()
 
-router_publication_file.get('/', get_all_publication_file)
-router_publication_file.get('/:id_publication', get_publication_file)
-router_publication_file.post('/add', add_publication_file)
-router_publication_file.delete('/delete/:id_file/:id_publication', delete_publication_file)
-router_publication_file.put('/update/:id_file/:id_publication', update_publication_file)
+router_publication_file.get('/', verify_Token, get_all_publication_file)
+router_publication_file.get('/:id_publication', verify_Token, get_publication_file)
+router_publication_file.post('/add', verify_Token, add_publication_file)
+router_publication_file.delete('/delete/:id_file/:id_publication', verify_Token, delete_publication_file)
+router_publication_file.put('/update/:id_file/:id_publication', verify_Token, update_publication_file)
 
 
 module.exports = router_publication_file
--- backend/routes/user.routes.js
+++ backend/routes/user.routes.js
@@ -1,12 +1,13 @@
 const { get_all_user, get_user, add_user, delete_user, update_user } = require('../controllers/user.controller')
+const { verify_Token, verifyAdminRole } = require('../middlewares/auth.middleware')
 
 const router_user = require('express').Router()
 
-router_user.get('/', get_all_user)
-router_user.get('/:id', get_user)
+router_user.get('/', verify_Token, verifyAdminRole, get_all_user)
+router_user.get('/:id', verify_Token, verifyAdminRole, get_user)
 router_user.post('/add', add_user)
-router_user.delete('/delete/:id', delete_user)
-router_user.put('/update/:id', update_user)
+router_user.delete('/delete/:id', verify_Token, delete_user)
+router_user.put('/update/:id', verify_Token, update_user)
 
 
 module.exports = router_user  
